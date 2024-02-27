6f2ba92ee2edc5e97b52512812f2673b0549c3f2
bl3rt4 <blerta.aazemi@gmail.com>
2024-02-27 13:09:33 +0100

    SignUpView

---
+++ FirstAid/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  FirstAid
+//
+//  Created by Macbook Air on 2/27/24.
+//  Copyright © 2024 MacBook Air. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet var Email:UILabel!
+    @IBOutlet var password:UILabel!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
