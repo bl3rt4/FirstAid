62355f50affb6d844423260a0480cd9b410e58ce
bl3rt4 <blerta.aazemi@gmail.com>
2024-02-27 23:07:59 +0100

    databaza

--- FirstAid/LoginViewController.swift
+++ FirstAid/LoginViewController.swift
@@ -7,18 +7,70 @@
 //
 
 import UIKit
+import CoreData
 
 class LoginViewController: UIViewController {
-    @IBOutlet var Email:UILabel!
-    @IBOutlet var password:UILabel!
-   
+
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    
+    func openAlert(message: String) {
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func loginClicked(_ sender: UIButton) {
+        guard let email = emailTextField.text, !email.isEmpty else{
+            openAlert(message: "Please enter an email")
+            return
+        }
+        
+        guard let password = passwordTextField.text, !password.isEmpty else{
+            openAlert(message: "Please enter a password")
+            return
+        }
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext // Access context from app delegate
 
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "UserEntity") // Replace with entity name
+        fetchRequest.predicate = NSPredicate(format: "email = %@ AND password = %@", email, password)
+
+        do {
+            let results = try context.fetch(fetchRequest)
+            
+            if let user = results.first as? NSManagedObject {
+                // User found! Redirect to list view
+
+                //let listViewController = ListViewController()
+                //self.navigationController?.pushViewController(listViewController, animated: false)
+            } else {
+                // User not found
+                openAlert(message: "Invalid email or password")
+            }
+            
+        } catch {
+            // Handle any errors during fetch operation
+            print("Error fetching user: \(error)")
+            openAlert(message: "An error occurred. Please try again.")
+        }
+    }
+   
+
+        
+
+    
+    @IBAction func signupClicked(_ sender: UIButton) {
+    }
     /*
     // MARK: - Navigation
 
--- FirstAid/SignupViewController.swift
+++ FirstAid/SignupViewController.swift
@@ -10,11 +10,72 @@ import UIKit
 
 class SignupViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    
+    func openAlert(message: String) {
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func signupClicked(_ sender: UIButton) {
+        guard let firstName = firstNameTextField.text, !firstName.isEmpty else{
+                   openAlert(message: "Please enter a Name")
+                   return
+               }
+        
+        guard let lastName = lastNameTextField.text, !lastName.isEmpty else{
+                   openAlert(message: "Please enter a Last Name")
+                   return
+               }
+        guard let email = emailTextField.text, !email.isEmpty else{
+                   openAlert(message: "Please enter an email")
+                   return
+               }
+        guard let password = passwordTextField.text, !password.isEmpty else{
+                   openAlert(message: "Please enter a password")
+                   return
+               }
+        
+        //DB
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let userEntity = UserEntity(context: context)
+        userEntity.firstName = firstName
+        userEntity.lastName = lastName
+        userEntity.email = email
+        userEntity.password = password
+
+        do {
+            try context.save()
+            // Create an alert to inform the user
+            openAlert(message: "You are now registered")
+
+            //self.present(alert, animated: true, completion: nil)
+        }
+        catch{
+            print("User saving error: ", error)
+        }
+        
+        
+    }
+
+    
+    @IBAction func loginClicked(_ sender: UIButton) {
+    }
+    @IBAction func backClicked(_ sender: UIButton) {
+    }
+    
+    
     
 
     /*
