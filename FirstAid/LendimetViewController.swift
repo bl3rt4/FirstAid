11389d8fd232026667de96ad24d7a5d64a033975
dafiinaa <dafinaasadiku@gmail.com>
2024-02-28 21:13:43 +0100

    LendimetViewController

---
+++ FirstAid/LendimetViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LendimetViewController.swift
+//  FirstAid
+//
+//  Created by Macbook Air on 2/28/24.
+//  Copyright © 2024 MacBook Air. All rights reserved.
+//
+
+import UIKit
+
+class LendimetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
